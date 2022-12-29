229de7876aa3763a0e02dab9ebab0c19700796eb
Microled <microled@gmail.com>
2022-12-29 13:16:42 +0100

    002 - Cajas de Texto

--- bbdd_sqllite.py
+++ bbdd_sqllite.py
@@ -1,13 +1,21 @@
+# VENTANA BÁSICA CON PYTHON
+# TEXTO
+# IMPORTAMOS MODULO
+
 from tkinter import *
 # Variables
 ANCHO="550"
-ALTO="400"
+ALTO="540"
 POSX="500"
 POSY="150"
 
+colorFondo="#007"
+colorLetra="#FFF"
+colorVentana="blue"
+
 #ventana
 ventana =Tk()
-ventana.config(bg="red")
+ventana.config(bg=colorVentana)
 ventana.geometry(ANCHO+"x"+ALTO+"+"+POSX+"+"+POSY)
 ventana.title("Ventana de Python")
 
@@ -19,7 +27,13 @@ edad=IntVar()
 etiquetaNombre=Label(ventana, text="Nombre").place(x=50, y=50)
 cajaNombre=Entry(ventana, textvariable=nombre).place(x=110, y=50)
 
+etiquetaTexto=Label(ventana, text="Texto amplio", bg=colorFondo, fg=colorLetra).place (x=50, y=130)
+
 etiquetaEdad=Label(ventana, text="Edad").place(x=50, y=90)
 cajaEdad=Entry(ventana, textvariable=edad).place(x=110, y=90)
+
+#Text
+text=Text(ventana)
+text.place(x=130, y=130, width=380)
 
 ventana.mainloop()
